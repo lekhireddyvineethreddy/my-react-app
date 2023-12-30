50208447f1ad629e7e39eb00a8ee799daa45969c
lekhireddyvineethreddy <149251683+lekhireddyvineethreddy@users.noreply.github.com>
2023-12-30 21:56:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./Expenseitem";
+
+function App() {
+  return (
+    <div>
+      <h2>  </h2>
+      <ExpenseItem>  </ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
